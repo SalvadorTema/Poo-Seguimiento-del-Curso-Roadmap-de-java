9e0f4344df0d8030febf572369403e69be95c423
Salvador <stemas@miumg.edu.gt>
2026-05-15 19:40:43 -0700

    enumeration

---
+++ src/enumeration/EjemploDeEnum.java
@@ -0,0 +1,15 @@
+package enumeration;
+
+public class EjemploDeEnum {
+	
+	enum Size {
+		   SMALL, MEDIUM, LARGE, EXTRALARGE
+		}
+	
+	public static void main(String[] args) {
+		
+	      System.out.println(Size.SMALL);
+	      System.out.println(Size.MEDIUM);
+	}
+
+}
